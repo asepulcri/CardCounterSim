7828028687e4c8587df0feab2182a042987797bf
asepulcri <asepulcri@tudelft.nl>
2024-07-19 15:35:36 +0200

    Linked SDL2

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,12 @@
+#include <SDL2/SDL.h>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "Error" << SDL_GetError();
+    }
+    else{
+        std::cout << "Hello, World!" << std::endl;
+    }
     return 0;
 }
